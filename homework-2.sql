49b3439f8dea8db54e6023cea2359aeaefebe3df
ViolinaS <110231840+ViolinaS@users.noreply.github.com>
2022-10-31 02:03:16 +0300

    Add files via upload

--- homework-2.sql
+++ homework-2.sql
@@ -34,3 +34,35 @@ collection_id INTEGER REFERENCES Collection(id),
 CONSTRAINT pk PRIMARY KEY (music_tracks_id, collection_id)
 );
 
+-- Исправления связей в таблицах
+
+ALTER TABLE Singer 
+DROP COLUMN genre_id CASCADE;
+
+ALTER TABLE Singer 
+DROP COLUMN album_id CASCADE;
+
+-- Добавление новых таблиц для связи "многие ко многим"
+
+CREATE TABLE singer_genres (
+genres_id INTEGER REFERENCES Genres(id),
+singer_id INTEGER REFERENCES Singer(id),
+CONSTRAINT genres_singer_pkey PRIMARY KEY (genres_id, singer_id) -- связь между жанрами и исполнителями
+);
+
+CREATE TABLE album_singer (
+singer_id INTEGER REFERENCES Singer(id),
+album_id INTEGER REFERENCES Album(id),
+CONSTRAINT pk_singer_album PRIMARY KEY (singer_id, album_id) -- связь между исполнителями и альбомами
+);
+
+-- Исправления типа поля в Music_Tracks(duration)
+ALTER TABLE Music_Tracks
+DROP COLUMN duration;
+
+ALTER TABLE Music_Tracks
+ADD COLUMN duration time;
+
+-- Исправление имени констрейнта в Tracks_collection
+ALTER TABLE Tracks_collection
+RENAME CONSTRAINT pk TO pk_tracks_collection;
